2f67878eced0d1040f5eafecf9fed8dd2ed5c899
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-17 07:56:13 +0530

    Initial commit

--- fibonaccisequence.py
+++ fibonaccisequence.py
@@ -5,8 +5,8 @@ a = 0
 b = 1
 c = 0
 for i in range(0,n):            # 0,1,2,3,4,5
-    print(a,end=" ")            # 0 1 1 2 3
-    d=a+b
-    a=b
-    b=d
-    c=c+1
+    print(a,end=" ")            # Output - 0 1 1 2 3
+    d=a+b                       # 1,2,3,5,8
+    a=b                         # 1,1,2,3,5
+    b=d                         # 1,2,3,5,8
+    c=c+1                       # 1,2,3,4,5
