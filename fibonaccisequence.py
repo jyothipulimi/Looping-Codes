4fe7a6547457656fe75e7f8f03fd1a479411f639
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-17 07:44:12 +0530

    Initial commit

--- fibonaccisequence.py
+++ fibonaccisequence.py
@@ -4,7 +4,7 @@ n = int(input("Enter n: "))     # 5
 a = 0
 b = 1
 c = 0
-for i in range(c,n):            # 0,1,2,3,4,5
+for i in range(0,n):            # 0,1,2,3,4,5
     print(a,end=" ")            # 0 1 1 2 3
     d=a+b
     a=b
