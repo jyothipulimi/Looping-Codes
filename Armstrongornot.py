c4e03a0eece2d26a9a9d27ef029297825b58a67b
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-18 20:18:08 +0530

    Initial commit

--- Armstrongornot.py
+++ Armstrongornot.py
@@ -2,12 +2,17 @@
 
 n = int(input("Enter n: "))     # 153
 m = n                           # 153
+rem = n
+count = 0
 sum = 0                         # 0
+while rem > 0:
+    count += 1                  # 1,2,3
+    rem = rem//10               # 15,1,0
 while m>0:
     d = m%10                    # 3, 5,1
-    sum = sum+d*d*d             # 27,152,153
+    sum = sum+d**count          # 27,152,153
     m=m//10                     # 15,1,0
-if sum == n:                    # 153 == 153 -- True
+if sum == n:                    # 153 ==153 -- True
     print("Armstrong")          # Armstrong
 else:
     print("Not Armstrong")
