ca9650c10d7ad30cfd1592c4df4108c60321bd3b
Jyothi <pulimi.jyothi123@gmail.com>
2026-03-10 17:11:08 +0530

    "Initial commit"

--- fibonaccisequence.py
+++ fibonaccisequence.py
@@ -1,12 +1,24 @@
 # To Print Fibonacci sequence
-
+'''
 n = int(input("Enter n: "))     # 5
 a = 0
 b = 1
 c = 0
-for i in range(0,n):            # 0,1,2,3,4,5
+for i in range(0,n):            # 0,1,2,3,4
     print(a,end=" ")            # Output - 0 1 1 2 3
     d=a+b                       # 1,2,3,5,8
     a=b                         # 1,1,2,3,5
     b=d                         # 1,2,3,5,8
     c=c+1                       # 1,2,3,4,5
+
+'''
+
+n = int(input("Enter n: "))
+a = 0
+b = 1
+print(a,b,end=' ')
+for i in range(2,n):
+    c=a+b
+    print(c,end=' ')
+    a=b
+    b=c`
--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,5 @@
 # Palindrome
-"""
+
 n = int(input("Enter n: "))     # 121                   # 1223
 m = n                           # m=121                 # 1223
 s = 0                           # 0                     # 0
@@ -7,7 +7,7 @@ while n>0:                      # 121 > 0 -- True       # 1223 > 0
     d=n%10                      # 1,2,1                 # 3,2,2,1
     s=s*10+d                    # 1,12,121              # 3,32,322,3221
     n=n//10                     # 12,1,0                # 122,12,1,0
-if s==m:                        # 121 == 121 -- True    # 3221 == 1223 -- False
+if s==m:                        # 121 == 121 -- True     # 3221 == 1223 -- False
     print("Palindrome")         # Palindrome
 else:
     print("Not a Palindrome")                           # Not a Palindrome
@@ -22,3 +22,4 @@ if rev == n:                    # madam == madam -- True
     print("Palindrome")         # Palindrome
 else:
     print("Not a Palindrome")
+"""
